f729a9eccf2739b1b04b776285c651c39a5d63df
Kevin Gruber <kevin-gruber1@gmx.at>
2018-11-29 14:30:47 +0100

    finished iteratorChallenge

---
+++ IteratorChallenge/IteratorChallenge.py
@@ -0,0 +1,11 @@
+
+itemList = ["Milk", "Apple", "Orange", "Rise", "Banana"]
+
+# iteratorList = iter(itemList)
+#
+# for item in range(0, len(itemList)):
+# 	nextItem = next(iteratorList)
+# 	print(nextItem)
+
+for item in itemList:
+	print(item)
